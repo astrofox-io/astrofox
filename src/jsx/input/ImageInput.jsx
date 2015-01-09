267f697b0a95f5c3e34762841ee7b9be0f0d121e
Mike Cao <mike@mikecao.com>
2015-01-08 22:27:26 -0800

    Open file dialog for image input.

--- src/jsx/input/ImageInput.jsx
+++ src/jsx/input/ImageInput.jsx
@@ -34,28 +34,11 @@ var ImageInput = React.createClass({
 
         var file = e.dataTransfer.files[0];
 
-        if (/^image/.test(file.type)) {
-            var reader = new FileReader();
-            var timer = AstroFox.getTimer();
+        this.loadFile(file);
+    },
 
-            reader.onload = function(fe) {
-                // DEBUG
-                console.log('image loaded', timer.get('image_load'));
-                var data = fe.target.result;
-
-                this.setState({ src: data }, function() {
-                    this.image.src = this.refs.image.getDOMNode().src = this.state.src;
-                    this.props.onChange(this.props.name, this.image);
-                });
-            }.bind(this);
-
-            timer.set('image_load');
-
-            reader.readAsDataURL(file);
-        }
-        else {
-            alert('Invalid image.');
-        }
+    handleClick: function(e) {
+        this.refs.file.getDOMNode().click();
     },
 
     handleDelete: function(e) {
@@ -63,9 +46,41 @@ var ImageInput = React.createClass({
         e.preventDefault();
 
         this.image.src = '';
+        this.refs.form.getDOMNode().reset();
+
         this.setState({ src: '' }, function() {
             this.props.onChange(this.props.name, null);
         }.bind(this));
+    },
+
+    handleFileOpen: function(e) {
+        e.preventDefault();
+
+        if (e.target.files.length > 0) {
+            this.loadFile(e.target.files[0]);
+        }
+    },
+
+    loadFile: function(file) {
+        if (!(/^image/.test(file.type))) return;
+
+        var reader = new FileReader();
+        var timer = AstroFox.getTimer();
+
+        reader.onload = function(fe) {
+            // DEBUG
+            console.log('image loaded', timer.get('image_load'));
+            var data = fe.target.result;
+
+            this.setState({ src: data }, function() {
+                this.image.src = this.refs.image.getDOMNode().src = this.state.src;
+                this.props.onChange(this.props.name, this.image);
+            });
+        }.bind(this);
+
+        timer.set('image_load');
+
+        reader.readAsDataURL(file);
     },
 
     render: function() {
@@ -75,13 +90,21 @@ var ImageInput = React.createClass({
             <div>
                 <div className="input input-image"
                     onDrop={this.handleDrop}
-                    onDragOver={this.handleDragOver}>
+                    onDragOver={this.handleDragOver}
+                    onClick={this.handleClick}>
+                    <form ref="form" className="input-file">
+                        <input
+                            ref="file"
+                            type="file"
+                            onChange={this.handleFileOpen}
+                        />
+                    </form>
                     <img ref="image" style={style} />
                 </div>
                 <div className="input input-image-delete icon-cancel"
                     onClick={this.handleDelete}
-                    style={style}>
-                </div>
+                    style={style}
+                />
             </div>
         );
     }
