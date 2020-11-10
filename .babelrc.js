b1169eee0b520de4ac3bb31aadfc45ab7f476a2b
Derek Ziemba <derekziemba@gmail.com>
2020-11-10 14:34:03 -0600

    convert babelrc to es5 js module

--- .babelrc.js
+++ .babelrc.js
@@ -1,37 +1,30 @@
-{
-  "presets": [
+module.exports = function (api) { // Using .babelrc JS example from https://babeljs.io/docs/en/configuration#babelconfigjson
+  api.cache(true);
+
+  const PRODUCTION = (process.env.BABEL_ENV || process.env.NODE_ENV) === 'production'; // Adapted from https://github.com/babel/babel/issues/5276
+
+  const presets = [
     ["@babel/env", {
-      "targets": {
-        "electron": "10.1.5"
+      targets: {
+        electron: "10.1.5"
       }
     }],
     "@babel/react"
-  ],
-  "plugins": [
-    "react-refresh/babel",
+  ];
+
+  const plugins = [
     "@babel/proposal-class-properties",
     "@babel/proposal-object-rest-spread",
     "@babel/proposal-optional-chaining",
     "@babel/proposal-nullish-coalescing-operator",
     "@babel/proposal-export-default-from"
-  ],
-  "env": {
-    "production": {
-      "presets": [
-        ["@babel/env", {
-          "targets": {
-            "electron": "10.1.5"
-          }
-        }],
-        "@babel/react"
-      ],
-      "plugins": [
-        "@babel/proposal-class-properties",
-        "@babel/proposal-object-rest-spread",
-        "@babel/proposal-optional-chaining",
-        "@babel/proposal-nullish-coalescing-operator",
-        "@babel/proposal-export-default-from"
-      ]
-    }
-  }
+  ];
+
+  // Workaround Error: [BABEL] React Refresh Babel transform should only be enabled in development environment.
+  if (!PRODUCTION) { plugins.push("react-refresh/babel"); }
+
+  return {
+    presets,
+    plugins
+  };
 }
