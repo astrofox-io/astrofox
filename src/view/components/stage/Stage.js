5fcfc34a3170968a52d286862b848b8f62400e66
Mike Cao <mike@mikecao.com>
2021-04-06 21:32:44 -0700

    Fixed more react-spring transitions.

--- src/view/components/stage/Stage.js
+++ src/view/components/stage/Stage.js
@@ -54,29 +54,28 @@ export default function Stage() {
 }
 
 const Loading = ({ show }) => {
-  const transitions = useTransition(show, 0, {
+  const transitions = useTransition(show, {
     from: { opacity: 0, width: '200px', height: '200px', margin: '-100px 0 0 -100px' },
     enter: { opacity: 1, width: '100px', height: '100px', margin: '-50px 0 0 -50px' },
     leave: { opacity: 0, width: '200px', height: '200px', margin: '-100px 0 0 -100px' },
   });
 
-  return transitions.map(
-    ({ item, key, props }) =>
-      item && <animated.div key={key} className={styles.loading} style={props} />,
+  return transitions(
+    (style, item) => item && <animated.div className={styles.loading} style={style} />,
   );
 };
 
 const RenderInfo = ({ show, onClose }) => {
-  const transitions = useTransition(show, 0, {
+  const transitions = useTransition(show, {
     from: { opacity: 0, maxHeight: 0 },
     enter: { opacity: 1, maxHeight: 100 },
     leave: { opacity: 0, maxHeight: 0 },
   });
 
-  return transitions.map(
-    ({ item, key, props }) =>
+  return transitions(
+    (style, item) =>
       item && (
-        <animated.div key={key} className={styles.renderInfo} style={props}>
+        <animated.div className={styles.renderInfo} style={style}>
           <RenderPanel onClose={onClose} />
         </animated.div>
       ),
