b933244f67275961a73c43f8d587f45b218e27d7
Mike Cao <moocao@gmail.com>
2026-02-04 08:12:09 -0800

    refactor: move electron builds to vite

--- webpack.config.electron.js
+++
@@ -1,4 +0,0 @@
-const main = require('./webpack.config.main.js');
-const preload = require('./webpack.config.preload');
-
-module.exports = [main, preload];
--- webpack.config.js
+++
@@ -1,5 +0,0 @@
-const view = require('./webpack.config.view.js');
-const main = require('./webpack.config.main.js');
-const preload = require('./webpack.config.preload');
-
-module.exports = [view, main, preload];
